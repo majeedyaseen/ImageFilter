829636a2d7bcdc8f17bf5220daea0951302e1cb9
Yaseen Majeed <majeed@trialx.com>
2017-12-11 20:40:36 +0530

    Initial Commit

---
+++ ImageFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageFilter
+//
+//  Created by Yaseen Majeed on 11/12/17.
+//  Copyright © 2017 Yaseen Majeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
